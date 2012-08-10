1fce416f7ed3581a61db9f1126f29fe9bff263c2
Steven Paul Sanderson II <spsanderson@gmail.com>
2012-08-10 09:30:57 -0400

    added biogen, bmc software, broadcom

--- stocktwitterdb.py
+++ stocktwitterdb.py
@@ -51,4 +51,5 @@ if __name__ == '__main__':
                              'amgen','$APOL','#APOL','apollo group','$AAPL','#AAPL','apple','$AMAT','#AMAT',
                              'applied materials','$ADSK','#ADSK','Autodesk','$ADP','#ADP','Automatic Data Processing',
                              '$AVGO','#AVGO','Avago Technologies','$BIDU','#BIDU','Badiu','$BBBY','#BBBY','Bed Bath and Beyond',
-                             'Bed Bath & Beyond'])
+                             'Bed Bath & Beyond','$BIIB','#BIIB','Biogen','$BMC','#BMC','BMC Software','$BRCM','#BRCM',
+                             'Braodcom'])
